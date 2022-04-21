251d20295df5b223fb47327293a4072e568f7e0e
Kornel Gładkowski <s22411@pjwstk.edu.pl>
2022-04-21 10:01:14 +0200

    commit

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Models;
 using Models.DTOs;
 using Service;
